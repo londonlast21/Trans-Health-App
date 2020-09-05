869623edc05ce25ed7f69a68dce57ddc56f5918c
jack <biffisbuffm@gmail.com>
2020-09-05 09:04:26 -0500

    edit html

--- controllers/home-routes.js
+++ controllers/home-routes.js
@@ -1,7 +1,20 @@
 const router = require('express').Router();
+const sequelize = require('../config/connection');
+const { Post, User, Comment } = require('../models');
+
 
 router.get('/', (req, res) => {
-    res.send'homepage');
+    res.render('homepage', {
+        id: 1,
+        name: 'Dr. Shiri Raphaely',
+        location: 'UW Health',
+        user_id: "Jack",
+        created_at: new Date(),
+        comments: [{}, {}],
+        user: {
+            username: 'test_user'
+        }
+    });
 });
 
 module.exports = router;
