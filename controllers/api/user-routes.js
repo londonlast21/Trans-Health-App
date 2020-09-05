8bc4f0fb04fde8c510045dcaa1a3856579d9965f
jack <biffisbuffm@gmail.com>
2020-09-05 18:20:48 -0500

    testing

--- controllers/api/user-routes.js
+++ controllers/api/user-routes.js
@@ -138,15 +138,13 @@ router.delete('/:id', (req, res) => {
 router.post('/logout', (req, res) => {
     if (req.session.loggedIn) {
         req.session.destroy(() => {
-            res.status(204).end();
-            console.log("hi");
+          res.status(204).end();
         });
-    }
-    else {
+      }
+      else {
         res.status(404).end();
-    }
+      }
+
 });
-
-
 
 module.exports = router;
