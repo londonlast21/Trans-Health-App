563c40da38b0dd7388a10b65a6adad995f220951
jack <biffisbuffm@gmail.com>
2020-09-05 15:37:27 -0500

    log in

--- controllers/api/index.js
+++ controllers/api/index.js
@@ -5,9 +5,9 @@ const postRoutes = require('./post-routes.js');
 const commentRoutes = require('./comment-routes.js');
 
 
+
 router.use('/posts', postRoutes);
 router.use('/users', userRoutes);
 router.use('/comments', commentRoutes);
 
 module.exports = router;
-//comment
