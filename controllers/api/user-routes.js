46781f33667e20ef5fd49cc620c8f31d6e511c36
jack <biffisbuffm@gmail.com>
2020-09-05 14:18:09 -0500

    logout

--- controllers/api/user-routes.js
+++ controllers/api/user-routes.js
@@ -137,6 +137,7 @@ router.post('/logout', (req, res) => {
     if (req.session.loggedIn) {
         req.session.destroy(() => {
             res.status(204).end();
+            console.log("hi");
         });
     }
     else {
