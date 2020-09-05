975ef1b94102d6c44a880a8353f8cb1843cc2e70
jack <biffisbuffm@gmail.com>
2020-09-05 13:50:16 -0500

    user id

--- controllers/home-routes.js
+++ controllers/home-routes.js
@@ -67,7 +67,50 @@ router.get('/login', (req, res) => {
     res.render('login');
 })
 
+// test for single post page
+router.get('/post/:id', (req, res) => {
+    Post.findOne({
+        where: {
+            id: req.params.id
+        },
+        attributes: [
+            'id',
+            'name',
+            'location',
+            'user_id'
+        ],
+        include: [
+            {
+            model: Comment,
+            attributes: ['id', 'comment_text', 'post_id', 'user_id'],
+            include: {
+                model: User,
+                attributes: ['username']
+            }
+         },
+         {
+             model: User,
+             attributes: ['username']
+         }
+            
+        ]
+    })
+    .then(dbPostData => {
+        if (!dbPostData) {
+            res.status(404).json({ message: 'No post matching this id' });
+            return;
+        }
 
+        const post = dbPostData.get({ plain: true });
+    
+
+         res.render('single-post', { post });
+    })
+        .catch(err => {
+        console.log(err);
+        res.status(500).json(err);
+    });
+});
 
     
 module.exports = router;
