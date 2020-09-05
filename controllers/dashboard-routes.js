a4c46072383d4d9cce9ae3062a26f27b13bbf88a
jack <biffisbuffm@gmail.com>
2020-09-05 15:57:28 -0500

    edit

--- controllers/dashboard-routes.js
+++ controllers/dashboard-routes.js
@@ -4,7 +4,40 @@ const { Post, User, Comment } = require('../models');
 
 
 router.get('/', (req, res) => {
-    res.render('dashboard', { loggedIn: true });
+    Post.findAll({
+        where: {
+            user_id: req.session.user_id
+        },
+        attributes: [
+            'id',
+            'name',
+            'location',
+            'created_at'
+        ],
+        include: [
+            {
+                model: Comment,
+                attributes: ['id', 'comment_text', 'post_id', 'user_id', 'created_at'],
+                include: {
+                    model: User,
+                    attributes: ['username']
+                }
+            },
+            {
+                model: User,
+                attributes: ['username']
+            }
+
+        ]
+    })
+    .then(dbPostData => {
+        const posts = dbPostData.map(post => post.get({ plain: true }));
+        res.render('dashboard', { posts, loggedIn: true });
+    })
+    .catch(err => {
+        console.log(err);
+        res.status(500).json(err);
+    })
   });
 
 module.exports = router;
