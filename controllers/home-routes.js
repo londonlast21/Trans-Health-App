0f6b0a330a08ebc9f1fd8b7e7f5a821fc3cd45b0
jack <biffisbuffm@gmail.com>
2020-09-05 10:45:35 -0500

    homepage loads

--- controllers/home-routes.js
+++ controllers/home-routes.js
@@ -3,37 +3,57 @@ const sequelize = require('../config/connection');
 const { Post, User, Comment } = require('../models');
 
 
-router.get('/', (req, res) => {
-  Post.findAll({
-      attributes: [
-          'id',
-          'name',
-          'location',
-          'created_at',
-          'user_id'
-          
+// router.get('/', (req, res) => {
+//     res.render('homepage', {
+//         id: 1,
+//         name: 'Dr. Shiri Raphaely',
+//         location: 'UW Health',
+//         user_id: "Jack",
+//         created_at: new Date(),
+//         comments: [{}, {}],
+//         user: {
+//             username: 'test_user'
+//         }
+//     });
+// });
 
-      ],
-      include: [
-          {
-              model: Comment,
-              attributes: ['id', 'comment_text', 'post_id', 'user_id', 'created_at'],
-              include: {
-                  model: User,
-                  attributes: ['username']
-              }
-          }
-      ]
-  })
-  .then(dbPostData => {
-      res.render('homepage', dbPostData[0])
-  })
-  .catch(err => {
-      console.log(err);
-      res.status(500).json(err);
+router.get('/', (req, res) => {
+    Post.findAll({
+        attributes: [
+            'id',
+            'name',
+            'location',
+            'user_id'
+        ],
+        include: [
+            {
+                model: Comment,
+                attributes:['id', 'comment_text', 'post_id', 'user_id'],
+                include: {
+                    model: User,
+                    attributes: ['username']
+                }
+            },
+            {
+                model: User, 
+                attributes: ['username']
+            }
+        ]
+    })
+    .then(dbPostData => {
+        // pass a single post object into the homepage template
+        console.log(dbPostData[0]);
+        const posts = dbPostData.map(post => post.get({ plain: true }));
+        res.render('homepage', { posts });
+      })
+      .catch(err => {
+        console.log(err);
+        res.status(500).json(err);
+      });
   });
 
 
-});
 
+
+    
 module.exports = router;
