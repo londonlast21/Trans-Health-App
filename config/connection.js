18208da2f8831f20afee4548eeea23535a5ca260
jack <biffisbuffm@gmail.com>
2020-09-07 18:27:11 -0500

    heroku test

--- config/connection.js
+++ config/connection.js
@@ -1,14 +1,14 @@
-const Sequelize = require('sequelize');
+const { Sequelize } = require("sequelize/types");
 
-require('dotenv').config();
+let sequelize;
 
-// create connection to our db
-console.log(process.env.DB_USER, process.env.DB_PW);
-const sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PW, {
-  
-  host: 'localhost',
-  dialect: 'mysql',
-  port: 3306
-});
+if (process.env.JAWSDB_URL) {
+    sequelize = new Sequelize(proccess.env.JAWSDB_URL);
 
-module.exports = sequelize;
+} else {
+    sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PW, {
+      host: 'localhost',
+      dialect: 'mysql',
+      port: 3306
+    });
+}
