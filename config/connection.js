fcef223021fbca6d84c31c49a332456c6d96fb24
jack <biffisbuffm@gmail.com>
2020-09-07 19:11:57 -0500

    edit

--- config/connection.js
+++ config/connection.js
@@ -2,8 +2,8 @@ const { Sequelize } = require("sequelize/types");
 
 let sequelize;
 
-if (process.env.JAWSDB_URL) {
-    sequelize = new Sequelize(proccess.env.JAWSDB_URL);
+if (process.env.JAWSDB_YELLOW_URL) {
+    sequelize = new Sequelize(proccess.env.JAWSDB_YELLOW_URL);
 
 } else {
     sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PW, {
