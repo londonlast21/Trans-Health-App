9a294fff881c1e1b28b972862d704d78dd4a89f3
jack <biffisbuffm@gmail.com>
2020-09-08 19:58:56 -0500

    mysql2

--- config/connection.js
+++ config/connection.js
@@ -12,7 +12,7 @@ if (process.env.JAWSDB_URL) {
 } else {
   sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PW, {
     host: 'localhost',
-    dialect: 'mysql',
+    dialect: 'mysql2',
     port: 3306
   });
 }
