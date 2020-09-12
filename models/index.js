2c6d8470150af7f47ff551f4c83d85db08a45ca6
jack <biffisbuffm@gmail.com>
2020-09-11 19:21:56 -0500

    heroku

--- models/index.js
+++ models/index.js
@@ -5,25 +5,12 @@ const Comment = require('./comment');
 
 
 // associations
-User.hasMany(Post, {
-    foreignKey: 'user_id'
-});
-Post.belongsTo(User, {
-    foreignKey: 'user_id'
-});
-Comment.belongsTo(User, {
-    foreignKey: 'user_id'
-
-});
-Comment.belongsTo(Post, {
-    foreignKey: 'post_id'
-});
-User.hasMany(Comment, {
-    foreignKey: 'user_id'
-});
-Post.hasMany(Comment, {
-    foreignKey: 'post_id'
-});
+User.hasMany(Post);
+Post.belongsTo(User);
+Comment.belongsTo(User);
+Comment.belongsTo(Post);
+User.hasMany(Comment);
+Post.hasMany(Comment);
 
 
 
