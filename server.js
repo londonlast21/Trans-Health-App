20a93e704ed1940f612fe3534e3fb7f668cc0329
jack <biffisbuffm@gmail.com>
2020-09-11 19:15:37 -0500

    heroku

--- server.js
+++ server.js
@@ -26,6 +26,6 @@ app.use(express.urlencoded({ extended: true }));
 app.engine('handlebars', hbs.engine);
 app.set('view engine', 'handlebars');
 app.use(routes);
-sequelize.sync({ force: false }).then(() => {
+sequelize.sync({ force: true }).then(() => {
     app.listen(PORT, () => console.log('Now listening'));
 });
